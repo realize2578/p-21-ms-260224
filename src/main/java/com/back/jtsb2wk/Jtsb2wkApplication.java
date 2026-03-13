efac6b5a7c30001d9ad80da96c5c4e15696a4c5d
남진우 <realize2578@gmail.com>
2026-03-13 19:52:58 +0900

    [feat]: init

---
+++ src/main/java/com/back/jtsb2wk/Jtsb2wkApplication.java
@@ -0,0 +1,13 @@
+package com.back.jtsb2wk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jtsb2wkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jtsb2wkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/jtsb2wk/Jtsb2wkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.jtsb2wk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jtsb2wkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
